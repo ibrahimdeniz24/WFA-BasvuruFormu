91fee623257b559f6e51652269f506f2121c4610
ibrahimdeniz24 <160463714+ibrahimdeniz24@users.noreply.github.com>
2024-02-19 16:59:31 +0300

    isBasvuruFormu
    İş Başvuru Formu

---
+++ WFAbasvuruFormu/WFAbasvuruFormu/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace WFAbasvuruFormu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnbasvur = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox2.Location = new System.Drawing.Point(281, 260);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(285, 64);
+            this.checkBox2.TabIndex = 0;
+            this.checkBox2.Text = "Evet";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // checkBox3
+            // 
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox3.Location = new System.Drawing.Point(281, 219);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(285, 64);
+            this.checkBox3.TabIndex = 0;
+            this.checkBox3.Text = "Biliyor";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // checkBox4
+            // 
+            this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox4.Location = new System.Drawing.Point(281, 178);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(285, 64);
+            this.checkBox4.TabIndex = 0;
+            this.checkBox4.Text = "Yapıldı";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox5.Location = new System.Drawing.Point(281, 137);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(285, 64);
+            this.checkBox5.TabIndex = 0;
+            this.checkBox5.Text = "Evet";
+            this.checkBox5.UseVisualStyleBackColor = true;
+                       // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(136, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "İŞ BAŞVURU FORMU";
+    
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(21, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Yaşınızı Giriniz :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+           
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(21, 279);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(239, 40);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Bilgisayar Mühendisliği :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+           
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(21, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(239, 40);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "İngilizce :";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+           
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(21, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(239, 40);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Askerlik Durumu :";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(21, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(239, 40);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "C# Programlama Dili :";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(281, 109);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnbasvur
+            // 
+            this.btnbasvur.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnbasvur.Location = new System.Drawing.Point(26, 322);
+            this.btnbasvur.Name = "btnbasvur";
+            this.btnbasvur.Size = new System.Drawing.Size(432, 61);
+            this.btnbasvur.TabIndex = 3;
+            this.btnbasvur.Text = "BAŞVUR";
+            this.btnbasvur.UseVisualStyleBackColor = true;
+            this.btnbasvur.Click += new System.EventHandler(this.btnbasvur_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 395);
+            this.Controls.Add(this.btnbasvur);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnbasvur;
+    }
+}
+
---
+++ WFAbasvuruFormu/WFAbasvuruFormu/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAbasvuruFormu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnbasvur_Click(object sender, EventArgs e)
+        {
+            int yas = Convert.ToInt32(textBox1.Text);
+            bool cSharp = checkBox5.Checked;
+            bool askerlik = checkBox4.Checked;
+            bool ingilizce = checkBox3.Checked;
+            bool Pc1 = checkBox2.Checked;
+
+            if (yas <30 && cSharp == true && askerlik == true && ingilizce == true && Pc1 == true)
+            {
+                MessageBox.Show("Başvuru Başarılı");
+            }
+
+            else
+            { MessageBox.Show("Başvuru Başarızıdır.!"); }
+
+
+        }
+
+
+
+    }
+}
